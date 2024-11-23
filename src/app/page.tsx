2ba6669a8a67274eec6fb7dd2b3538058934a2c9
Logan Allen <logandallen25@gmail.com>
2024-11-23 04:17:59 -0600

    Updated description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -105,10 +105,10 @@ export default async function Home() {
       <Container className="mt-24 sm:mt-32 md:mt-56">
         <FadeIn className="max-w-3xl">
           <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
-            Software Developer and Data Enthusiast
+            Data Engineer / Software Developer
           </h1>
           <p className="mt-6 text-xl text-neutral-600">
-            As a software developer and data enthusiast, I&apos;m focused on
+            As a data engineer and software developer, I&apos;m focused on
             building solutions that not only meet user needs but are also robust
             and reliable, ensuring they deliver value and functionality. My work
             in data revolves around extracting meaningful insights from complex
